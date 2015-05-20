adbd0ed5beb3193b3ffcce7e9da2352698cdf6fc
Ryan Haase <ryandhaase@gmail.com>
2015-05-20 16:23:06 -0400

    Finished creating fixed nav menu during scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,23 +22,24 @@ $(function() {
 
   var elementWatcher = scrollMonitor.create(navMenu);
 
+  elementWatcher.lock();
   // elementWatcher.visibilityChange(function() {
   //   console.log(elementWatcher.isFullyInViewport)
   // });
 
-  // elementWatcher.partiallyExitViewport(function() {
-  //   navMenu.addClass('fixed');
-  // });
-  // elementWatcher.enterViewport(function() {
-  //   navMenu.removeClass('fixed');
-  // });
-  elementWatcher.stateChange(function() {
-    if(elementWatcher.isFullyInViewport) {
-      navMenu.removeClass('fixed');
-      console.log("remove fixed");
-    } else {
-      navMenu.addClass('fixed');
-      console.log("added fixed");
-    }
+  elementWatcher.exitViewport(function() {
+    navMenu.addClass('fixed');
   });
+  elementWatcher.enterViewport(function() {
+    navMenu.removeClass('fixed');
+  });
+  // elementWatcher.stateChange(function() {
+  //   if(elementWatcher.isFullyInViewport) {
+  //     navMenu.removeClass('fixed');
+  //     console.log("remove fixed");
+  //   } else {
+  //     navMenu.addClass('fixed');
+  //     console.log("added fixed");
+  //   }
+  // });
 });
